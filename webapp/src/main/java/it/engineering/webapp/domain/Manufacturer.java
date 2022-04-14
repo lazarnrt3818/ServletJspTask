a25782f68930862a26a0a0f7cabe0a53e369902e
Lazar Spasojevic <spasojevic2110@gmail.com>
2022-04-14 10:04:18 +0200

    Added manufacturer to persistence.xml

--- webapp/src/main/java/it/engineering/webapp/domain/Manufacturer.java
+++ webapp/src/main/java/it/engineering/webapp/domain/Manufacturer.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Manufacturer {
@@ -17,6 +18,7 @@ public class Manufacturer {
 	private String cid;
 	private String address;
 
+	@ManyToOne
 	@JoinColumn(name = "ID_city")
 	private City city;
 
