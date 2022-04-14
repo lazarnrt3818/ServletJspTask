aa713e143216bbe005933b2b59553929a6d498d3
Lazar Spasojevic <spasojevic2110@gmail.com>
2022-04-14 09:57:22 +0200

    Added domain class Manufacturer

---
+++ webapp/src/main/java/it/engineering/webapp/domain/Manufacturer.java
@@ -0,0 +1,81 @@
+package it.engineering.webapp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class Manufacturer {
+	@Id
+	@Column(name = "ID_manufacturer")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String pib;
+	private String cid;
+	private String address;
+
+	@JoinColumn(name = "ID_city")
+	private City city;
+
+	public Manufacturer(String pib, String cid, String address, City city) {
+		super();
+		this.pib = pib;
+		this.cid = cid;
+		this.address = address;
+		this.city = city;
+	}
+
+	public Manufacturer() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPib() {
+		return pib;
+	}
+
+	public void setPib(String pib) {
+		this.pib = pib;
+	}
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Manufacturer [id=" + id + ", pib=" + pib + ", cid=" + cid + ", address=" + address + ", city=" + city
+				+ "]";
+	}
+
+}
